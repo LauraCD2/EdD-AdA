5bef3d5ecb0025a38acec0620c5325cdd5f96eba
Lali <lauracamiladiazdelgado@gmail.com>
2023-03-28 14:57:07 -0500

    Update Z. PROYECTO FINAL/Descripcion.py Delete X.PYTHONDAVRegrLiML/Proyecto final.pdf

---
+++ Z. PROYECTO FINAL/Descripcion.py
@@ -0,0 +1,14 @@
+#Pregunta:
+#¿Cómo promover e incentivar el conocimiento sobre seguridad de la
+#información en adolescentes?
+#Descripción:
+#En el mundo moderno el acceso a internet se ha convertido en un elemento
+#fundamental. Jóvenes y niños están interactuando con dispositivos móviles y
+#servicios de internet mucho más rápido que en años anteriores.
+#Una de las preocupaciones más grandes de los padres es el poder proteger la
+#información e integridad de sus hijos, y evitar que sean víctimas de los
+#diversos delitos que pueden cometerse en la red.
+#Se busca encontrar una forma didáctica, directa y eficaz para enseñar a los
+#adolescentes y a sus padres el uso correcto y responsable de dispositivos
+#electrónicos y las bases principales de seguridad de la información
+
