207f04e8485411724cbb1ec45edb99fd907d41b1
Lali <lauracamiladiazdelgado@gmail.com>
2023-04-27 07:26:26 -0500

    l

--- 6. CONSULTA LIBRERIA PYTHON/1. ConsejoSuperior.py
+++ 6. CONSULTA LIBRERIA PYTHON/1. ConsejoSuperior.py
@@ -1,10 +1,11 @@
 #En la universidad se efectuó la elección del representante de los estudiantes ante el Consejo Superior. Se presentaron 30 candidatos y cada uno se identificó con un número del 1 al 30. Asumiendo que los 5000 estudiantes de la universidad votaron, elabore un programa donde:
 #Imprima un listado de mayor a menor, según el número de votos obtenidos por cada candidato
+#total votos = 5000
 
 import numpy as np
 
-#vector con los votos de cada candidato
-votos= np.random.randint(0,5000,30)
+#vector con los votos de cada candidato, cantidad max de votos = 5000
+votos = np.random.randint(1,5000,30)
 
 #vector con los candidatos
 candidatos={}
