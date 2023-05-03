05f3e2fe2f0dcfcb2c936b115ded349e37e629e5
Lali <lauracamiladiazdelgado@gmail.com>
2023-05-03 11:54:03 -0500

    updated from github

--- 6. CONSULTA LIBRERIA PYTHON/ExplicacionBD.py
+++ 6. CONSULTA LIBRERIA PYTHON/ExplicacionBD.py
@@ -35,7 +35,20 @@ for numero in numeros:
 
 
 ####RANDOMIZADOR DE NOMBRES DE ESTUDIANTES
+import names
 
+# Generar 6500 nombres y apellidos
+full_names = []
+for i in range(6500):
+    full_name = names.get_full_name()
+    full_names.append(full_name)
+    #
+    full_names.append(full_name)
+
+# Escribir los nombres y apellidos en un archivo de texto
+with open('nombres_apellidos.txt', 'w') as f:
+    for name in full_names:
+        f.write(name + '\n')
 
 ####RANDOMIZADOR DE PROMEDIO
 import random
