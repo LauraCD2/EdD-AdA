9ef9c9a1a10156c11d5c7739e239f7bcbba6da80
Lali <lauracamiladiazdelgado@gmail.com>
2023-05-03 10:42:04 -0500

    summary:

--- 6. CONSULTA LIBRERIA PYTHON/EstudiantesUIS.py
+++ 6. CONSULTA LIBRERIA PYTHON/EstudiantesUIS.py
@@ -1,4 +1,4 @@
-#Suponga que en la UIS hay 6500 estudiantes. Por cada uno de ellos tenemos un registro con el código, nombre y promedio acumulado. Hacer el programa que:
+#importe el archivo de excel con la base de datos de estudiantes de la UIS (BD_EstudiantesUIS.xlsx)
 
 #Imprima el código y el nombre de los estudiantes de la carrera X (debe leerse el código de la carrera a listar) que tengan promedio acumulado igual o mayor a 4 y decir cuántos fueron.
 #Imprima el código y el nombre de los estudiantes que ingresaron antes de 1990 (codigo inicia por 1) y están condicionales (promedio entre 2.7 y 3.2).
@@ -6,15 +6,30 @@
 import numpy as np
 import pandas as pd
 
-#importe Base de Datos BD_EstudiantesUIS.csv
-estudiantes = pd.read_csv("BD_EstudiantesUIS.csv", sep = ";") #separador es ";"
+#importe Base de Datos BD_EstudiantesUIS.xlsx
+#import openpyxl as xl
 
-#Imprima el código y el nombre de los estudiantes de la carrera X que tengan promedio acumulado igual o mayor a 4 y decir cuántos fueron.
-#pida la carerra a listar
-print("ingrese nombre de la carrera a listar")
-carrera = input()
+#wb = xl.load_workbook('BD_EstudiantesUIS.xlsx')
+#sheet = wb['Hoja1']
 
-#consulte la base de datos y filtre los estudiantes de la carrera ingresada
-estudiantes_carrera = estudiantes[estudiantes["Carrera"] == carrera]
+#Imprima el código y el nombre de los estudiantes de la carrera X (debe leerse el código de la carrera a listar) que tengan promedio acumulado igual o mayor a 4 y decir cuántos fueron.
+# Pida el numero de la carrera a listar
+# de la base de datos, lea el codigo y el nombre de los estudiantes de la carrera ingresada que tengan promedio acumulado igual o mayor a 4
+# LA CARRERA SELECCIONADA corresponde a las dos primeras cifras de la columna A de la base de datos (codigo)
+#carrera = input("Ingrese el código de la carrera a listar: ")
 
-print ("Los estudiantes de la carrera", carrera, "que tienen promedio acumulado igual o mayor a 4 son:")
+# importar la base de datos
+df = pd.read_excel('BD_EstudiantesUIS.xlsx')
+
+# Imprimir el código y el nombre de los estudiantes de la carrera X que tengan promedio acumulado igual o mayor a 4
+carrera = input("Ingrese el código de la carrera a listar: ")
+
+estudiantes_carrera = df[df['codigo'].astype(str).str[:2] == carrera]  # seleccionar estudiantes de la carrera X
+estudiantes_aprobados = estudiantes_carrera[estudiantes_carrera['promedio'] >= 4]  # seleccionar estudiantes con promedio mayor o igual a 4
+
+print("Estudiantes de la carrera " + carrera + " con promedio acumulado igual o mayor a 4:")
+print(estudiantes_aprobados[['codigo', 'nombre']])  # imprimir código y nombre de los estudiantes
+
+# contar estudiantes
+num_estudiantes = len(estudiantes_aprobados)
+print("Total de estudiantes con promedio igual o mayor a 4: " + str(num_estudiantes))
