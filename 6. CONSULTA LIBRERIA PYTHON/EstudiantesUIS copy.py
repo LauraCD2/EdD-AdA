42f1ad3dd062834a2de7577657e62e72a327446c
Lali <lauracamiladiazdelgado@gmail.com>
2023-05-04 07:16:26 -0500

    sum

--- 6. CONSULTA LIBRERIA PYTHON/EstudiantesUIS copy.py
+++ 6. CONSULTA LIBRERIA PYTHON/EstudiantesUIS copy.py
@@ -4,14 +4,14 @@
 #2. Imprima el código y el nombre de los estudiantes que ingresaron antes de 1990 (codigo inicia por 1) y están condicionales (promedio entre 2.7 y 3.2).
 
 import numpy as np
-import pandas as pd
+#import pandas as pd
 import streamlit as st
 
 #Imprima el código y el nombre de los estudiantes de la carrera X (debe leerse el código de la carrera a listar) que tengan promedio acumulado igual o mayor a 4 y decir cuántos fueron.
 # Pida el numero de la carrera a listar, de la base de datos, lea los estudiantes de la carrera ingresada que tengan promedio acumulado igual o mayor a 4
 # LA CARRERA SELECCIONADA corresponde a las dos primeras cifras de la columna A de la base de datos (codigo)
 
-# importar la base de datos
+# importar la base de datos (este no funciona en streamlit) CON STREAMLIT
 uploaded_file = st.file_uploader(
     "BD_EstudiantesUIS.xlsx", accept_multiple_files=False)
 if uploaded_file is not None:
@@ -22,21 +22,3 @@ else:
 
 # 1. Imprimir el código y el nombre de los estudiantes de la carrera X que tengan promedio acumulado igual o mayor a 4
 carrera = input("Ingrese el código de la carrera a listar: ")
-
-estudiantes_carrera = df[df['codigo'].astype(str).str[:2] == carrera]  # seleccionar estudiantes de la carrera X
-estudiantes_cuatro = estudiantes_carrera[estudiantes_carrera['promedio'] >= 4]  # seleccionar estudiantes con promedio mayor o igual a 4
-
-print("Estudiantes de la carrera " + carrera + " con promedio acumulado igual o mayor a 4:")
-print(estudiantes_cuatro[['codigo', 'nombre', 'promedio']])  # imprimir código y nombre de los estudiantes
-
-# contar estudiantes con promedio mayor o igual a 4 de la carrera X
-num_estudiantes = len(estudiantes_cuatro)
-print("Total de estudiantes con promedio igual o mayor a 4: " + str(num_estudiantes))
-
-#2. Imprimir el código y el nombre de los estudiantes que ingresaron antes de 1990 (la tercera cifra es 1 y la cuarta cifra menor que 9) y están condicionales (promedio entre 2.7 y 3.2).
-noventa = df[df['codigo'].astype(str).str[2] == '1']  # seleccionar estudiantes que ingresaron en el siglo XX
-# seleccionar estudiantes que ingresaron antes de 1990 y están condicionales
-novecond = noventa[(noventa['codigo'].astype(str).str[3] < '9') & (noventa['promedio'] >= 2.7) & (noventa['promedio'] <= 3.2)]
-
-print ("Estudiantes que ingresaron antes de 1990 y están condicionales:"+ "\n" + str(novecond[['codigo', 'nombre', 'promedio']]))
-print ("Total de estudiantes que ingresaron antes de 1990 y están condicionales: " + str(len(novecond)))
