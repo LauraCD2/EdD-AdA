d66656e440367c4380568970350ea085878e0007
Lali <lauracamiladiazdelgado@gmail.com>
2023-05-08 06:31:40 -0500

    non = int(input("Ingrese el numero de estudiantes: "))

--- 6. CONSULTA LIBRERIA PYTHON/EstudiantesUIS.py
+++ 6. CONSULTA LIBRERIA PYTHON/EstudiantesUIS.py
@@ -5,20 +5,14 @@
 
 import numpy as np
 import pandas as pd
-import streamlit as st
 
 #Imprima el código y el nombre de los estudiantes de la carrera X (debe leerse el código de la carrera a listar) que tengan promedio acumulado igual o mayor a 4 y decir cuántos fueron.
 # Pida el numero de la carrera a listar, de la base de datos, lea los estudiantes de la carrera ingresada que tengan promedio acumulado igual o mayor a 4
 # LA CARRERA SELECCIONADA corresponde a las dos primeras cifras de la columna A de la base de datos (codigo)
 
-# importar la base de datos
-#uploaded_file = st.file_uploader(
-    #"BD_EstudiantesUIS.xlsx", accept_multiple_files=False)
-#if uploaded_file is not None:
-    #file_name = uploaded_file
-#else:
-    #file_name = "BD_EstudiantesUIS.xlsx"
+#Importar la base de datos de estudiantes de la UIS
 df = pd.read_excel('BD_EstudiantesUIS.xlsx')
+#df = pd.read_excel('BD_EstudiantesUIS.xlsx', sheet_name='Hoja 1')
 
 # 1. Imprimir el código y el nombre de los estudiantes de la carrera X que tengan promedio acumulado igual o mayor a 4
 carrera = input("Ingrese el código de la carrera a listar: ")
