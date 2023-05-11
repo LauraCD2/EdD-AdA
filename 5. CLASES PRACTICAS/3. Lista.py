9bbc43979e9be112b3c1f48717cb6fb3f99b31fa
Lali <lauracamiladiazdelgado@gmail.com>
2023-05-11 07:25:39 -0500

    done first part of the exercise homework at 5. CLASES PRACTICAS/3. Lista.py

--- 5. CLASES PRACTICAS/3. Lista.py
+++ 5. CLASES PRACTICAS/3. Lista.py
@@ -1,3 +1,93 @@
 #crear clase lista e insertar nuevo nodo en sus distintas posiciones
 
-saabdjkasiidoias
+class Lista:
+    def __init__(self, cabeza=None):
+        self.cabeza = cabeza
+    
+    def insertar(self, nodo, posicion):
+        if posicion == 0:
+            nodo.asignar_sig(self.cabeza)
+            self.cabeza = nodo
+        else:
+            nodo_anterior = self.cabeza
+            nodo_actual = self.cabeza.siguiente
+            for i in range(1, posicion):
+                nodo_anterior = nodo_actual
+                nodo_actual = nodo_actual.siguiente
+            nodo_anterior.asignar_sig(nodo)
+            nodo.asignar_sig(nodo_actual)
+    
+    def imprimir(self):
+        nodo_actual = self.cabeza
+        while nodo_actual != None:
+            print(nodo_actual.valor)
+            nodo_actual = nodo_actual.siguiente
+    
+    def eliminar(self, posicion):
+        if posicion == 0:
+            self.cabeza = self.cabeza.siguiente
+        else:
+            nodo_anterior = self.cabeza
+            nodo_actual = self.cabeza.siguiente
+            for i in range(1, posicion):
+                nodo_anterior = nodo_actual
+                nodo_actual = nodo_actual.siguiente
+            nodo_anterior.asignar_sig(nodo_actual.siguiente)
+    
+    def buscar(self, valor):
+        nodo_actual = self.cabeza
+        posicion = 0
+        while nodo_actual != None:
+            if nodo_actual.valor == valor:
+                return posicion
+            else:
+                nodo_actual = nodo_actual.siguiente
+                posicion += 1
+        return -1
+    
+    def longitud(self):
+        nodo_actual = self.cabeza
+        longitud = 0
+        while nodo_actual != None:
+            longitud += 1
+            nodo_actual = nodo_actual.siguiente
+        return longitud
+    
+    def devolver_nodo(self, posicion):
+        nodo_actual = self.cabeza
+        for i in range(0, posicion):
+            nodo_actual = nodo_actual.siguiente
+        return nodo_actual
+    
+    def devolver_valor(self, posicion):
+        nodo_actual = self.cabeza
+        for i in range(0, posicion):
+            nodo_actual = nodo_actual.siguiente
+        return nodo_actual.valor
+    
+    def devolver_posicion(self, valor):
+        nodo_actual = self.cabeza
+        posicion = 0
+        while nodo_actual != None:
+            if nodo_actual.valor == valor:
+                return posicion
+            else:
+                nodo_actual = nodo_actual.siguiente
+                posicion += 1
+        return -1
+    
+    def cambiar_valor(self, posicion, nuevo_valor):
+        nodo_actual = self.cabeza
+        for i in range(0, posicion):
+            nodo_actual = nodo_actual.siguiente
+        nodo_actual.cambiar_valor(nuevo_valor)
+        
+    def cambiar_siguiente(self, posicion, nuevo_siguiente):
+        nodo_actual = self.cabeza
+        for i in range(0, posicion):
+            nodo_actual = nodo_actual.siguiente
+        nodo_actual.asignar_sig(nuevo_siguiente)
+        
+    def cambiar_cabeza(self, nueva_cabeza):
+        self.cabeza = nueva_cabeza
+        
